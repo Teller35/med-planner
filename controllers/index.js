66051501cf26f36ab8092866d37e0e7879e356a7
Teller35 <tellerwetzel@yahoo.com>
2021-06-16 16:12:31 -0600

    added landing page

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 
 // require routes
 const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
 
 // assign routes
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // export all router routes
 module.exports = router;
