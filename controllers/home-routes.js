e34fd2c581e88b28cc1f063105509762bfb74a93
Teller35 <tellerwetzel@yahoo.com>
2021-06-17 10:24:50 -0600

    signup page and caregiver page layouts done. improvement to landing page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,14 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage')
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+router.get('/signin', (req, res) => {
+    res.render('signin')
 })
 
 module.exports = router;
