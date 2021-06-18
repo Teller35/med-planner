2bccd8c7a0e6d3ba0820d416ebd69cf759f7c106
Teller35 <tellerwetzel@yahoo.com>
2021-06-18 14:16:13 -0600

    added javascript files for login, logout, signup, and edit-patient

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+async function patientLogin(event) {
+    event.preventDefault();
+    const email = document.querySelector('#email-signIn').value.trim();
+    const password = document.querySelector('#password-signIn').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/patients/login', {
+            method: 'post', body: JSON.stringify({ email, password }), headers: { 'Content-Type': 'application/json' }
+        })
+
+        if (response.ok) {
+            document.location.replace('/dashpatient');
+        }
+        else {
+            alert(response.statusText);
+        }
+    }
+}
+
+async function caregiverLogin(event) {
+    event.preventDefault();
+    const email = document.querySelector('#emailCare-signIn').value.trim();
+    const password = document.querySelector('#passwordCare-signIn').value.trim();
+
+    if (email && password) {
+        const response = await fetch('/api/caregivers/login', {
+            method: 'post', body: JSON.stringify({ email, password }), headers: { 'Content-Type': 'application/json' }
+        })
+
+        if (response.ok) {
+            document.location.replace('/dashcare');
+        }
+        else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.signIn-patient').addEventListener('submit', patientLogin);
+document.querySelector('.signIn-caregiver').addEventListener('submit', caregiverLogin);
