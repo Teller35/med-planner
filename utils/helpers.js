728849add88bb5f070ecbed3ed67b2c7c0960851
Amanda Hoshor <hoshor.manda@gmail.com>
2021-06-25 18:20:00 -0600

    added date and time formatting

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,11 +1,12 @@
 const luxon = require("../build/node/luxon");
 
+import { Time } from "gantt-schedule-timeline-calendar/dist/api/time";
 import { DateTime } from "luxon";
 
 global.DateTime = luxon.DateTime;
 
-DateTime.now().toFormat('LLL dd YY ')
-DateTime.now().toFormat('')
+DateTime.now().toFormat('LLL dd YY ');
+DateTime.now().toFormat('h mm');
 
 
   
@@ -14,5 +15,10 @@ module.exports = {
       return `${new Date(date).getMonth() + 1}/${new Date(
         date
       ).getDate()}/${new Date(date).getFullYear()}`;
+    },
+    format_time: (time) => {
+        return `${new Time(time).getHour() +1}/${new Hour(
+            hour
+        ).getHour()}/${new Hour(hour).getDate()}`
     },
 };
